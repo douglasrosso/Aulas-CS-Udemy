5ecf4a4ea9aa3a3e81579630d7ec301c1dd961a2
Douglas Martignago Rosso <douglasrosso10@gmail.com>
2022-11-22 17:12:34 -0300

    Att.

---
+++ 34- Quinto projeto/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _34__Quinto_projeto
+{
+    internal class Program
+    {
+        public struct DadosCadastraisStruct
+        {
+            public string Nome;
+            public DateTime DataDeNascimento;
+            public string NomeDaRua;
+            public UInt32 NumeroDaCasa;
+
+        }
+
+        public enum Resultado_e
+        {
+            Sucesso = 0,
+            Sair = 1,
+            Excecao = 2
+        }
+
+        public static void MostraMensagem(string mensagem)
+        {
+            Console.WriteLine(mensagem);
+            Console.WriteLine("Pressione qualquer tecla para continuar");
+            Console.ReadKey();
+            Console.Clear();
+        }
+
+        public static Resultado_e PegarString(ref string minhaString, string mensagem)
+        {
+            Resultado_e retorno;
+            Console.WriteLine(mensagem);
+            string temp = Console.ReadLine();
+            if (temp == "s" || temp == "S")
+            {
+                retorno = Resultado_e.Sair;
+            }
+            else
+            {
+                minhaString = temp;
+                retorno = Resultado_e.Sucesso;
+            }
+            Console.Clear();
+            return retorno;
+        }
+
+        public static Re
+
+
+        static void Main(string[] args)
+        {
+            List<DadosCadastraisStruct> ListaDeUsuarios = new List<DadosCadastraisStruct>();
+            string opcao = "";
+            do
+            {
+                Console.WriteLine("Digite C para cadastrar um nome usuário ou S para sair:");
+                opcao = Console.ReadKey(true).KeyChar.ToString().ToLower();
+                if (opcao == "c")
+                {
+                    // Cadastrar um novo usuário
+                }
+                else if (opcao == "s")
+                {
+                    // Sair da aplicação
+                    MostraMensagem("Encerrando o programa");
+                }
+                else
+                {
+                    // Opção desconhecida
+                    MostraMensagem("Opção desconhecida");
+                }
+            } while (opcao != "s");
+
+        }
+    }
+}
